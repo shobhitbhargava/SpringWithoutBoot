812cc3d827cd971842b899c5ca9d38d440d232d1
Shobhit Bhargava <shobhitbhargava@yahoo.com>
2024-07-05 11:42:34 +0530

    new commit - constructor injection

--- SpringProject/src/main/java/com/education/SpringProject/Alien.java
+++ SpringProject/src/main/java/com/education/SpringProject/Alien.java
@@ -1,5 +1,9 @@
 package com.education.SpringProject;
 
+import java.beans.ConstructorProperties;
+
+import javax.management.ConstructorParameters;
+
 public class Alien {
 	
 	private int age;
@@ -9,12 +13,29 @@ public class Alien {
 		System.out.println("Alien Object Created");
 	}
 	
+	public Alien(int age) {
+		System.out.println("Parameterized Age Alien Object Created");
+		this.age = age;
+	}
+	
+	public Alien(Laptop laptop) {
+		System.out.println("Parameterized Laptop Alien Object Created");
+		this.laptop = laptop;
+	}
+	
+	@ConstructorProperties({"age", "laptop"})
+	public Alien(int age, Laptop laptop) {
+		System.out.println("Parameterized Age, Laptop Alien Object Created");
+		this.age = age;
+		this.laptop = laptop;
+	}
+
 	public int getAge() {
 		return age;
 	}
 
 	public void setAge(int age) {
-		System.out.println("Setter Called");
+		System.out.println("Setter Called for Age");
 		this.age = age;
 	}
 
@@ -23,6 +44,7 @@ public class Alien {
 	}
 
 	public void setLaptop(Laptop laptop) {
+		System.out.println("Setter Called for Laptop");
 		this.laptop = laptop;
 	}
 
